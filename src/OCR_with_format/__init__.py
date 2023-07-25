e56f383904c75f1cc7d09fef8db025a96a8c9b3b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-07-25 18:03:12 +0200

    bump version

--- src/OCR_with_format/__init__.py
+++ src/OCR_with_format/__init__.py
@@ -12,7 +12,7 @@ bbox_regex = re.compile(r'bbox\s(\d+)\s(\d+)\s(\d+)\s(\d+)')
 confidence_regex = re.compile(r'x_wconf\s(\d+)')
 newlines_regex = re.compile(r'\n\s*\n')
 
-__version__ = "0.7"
+__version__ = "0.8"
 
 # msic
 
