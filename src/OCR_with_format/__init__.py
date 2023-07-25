4eb6d88135b669485dd7ef2d5e1a223f83ba0141
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-07-25 17:34:06 +0200

    bump version to 0.2

--- src/OCR_with_format/__init__.py
+++ src/OCR_with_format/__init__.py
@@ -12,7 +12,7 @@ bbox_regex = re.compile(r'bbox\s(\d+)\s(\d+)\s(\d+)\s(\d+)')
 confidence_regex = re.compile(r'x_wconf\s(\d+)')
 newlines_regex = re.compile(r'\n\s*\n')
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 # msic
 
