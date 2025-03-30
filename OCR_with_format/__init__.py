89a3154410624a88bd896c1a9a67a1b43723572b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-03-30 18:49:52 +0200

    fix: duplicate lines in output if using cli
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- OCR_with_format/__init__.py
+++ OCR_with_format/__init__.py
@@ -9,9 +9,7 @@ __VERSION__ = OCR_with_format.__VERSION__
 
 @beartype
 def cli() -> None:
-    out = fire.Fire(OCR_with_format().OCR)
-    print(out)
-    return
+    _ = fire.Fire(OCR_with_format().OCR)
 
 if __name__ == "__main__":
     cli()
